deb9893438e07b83ab2a5dd32db52f19c7e46537
Priyam Das <domidas01@gmail.com>
2023-05-31 02:45:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function(res){
+    return res.json();
+  })
+  .then(data => renderBooks(data));
   
 }
 
